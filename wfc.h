f5f1c8a61e16fa4934b1a6c7f6013c27f5ed71c3
Vladimir Pleskonjic <21056458+vplesko@users.noreply.github.com>
2023-05-26 07:50:05 +0100

    Use the pixel values directly instead of indexing them

--- wfc.h
+++ wfc.h
@@ -1,8 +1,11 @@
+#include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
 // @TODO implement
+// @TODO allow users to supply their own assert
+// @TODO allow users to supply their own malloc et al.
 
 int wfc_generate(
     int srcW, int srcH, const uint32_t *src,
@@ -22,42 +25,33 @@ int wfc_generatePixels(
     int bytesPerPixel,
     int srcW, int srcH, const unsigned char *src,
     int dstW, int dstH, unsigned char *dst) {
+    assert(bytesPerPixel >= 1 && bytesPerPixel <= 4);
+
     int ret = 0;
 
-    uint32_t *srcI = NULL;
-    uint32_t *dstI = NULL;
+    uint32_t *srcU32 = NULL;
+    uint32_t *dstU32 = NULL;
 
-    // @TODO if bytesPerPixel <= sizeof(int), each pixel can be simply cast to int
-    srcI = malloc(srcW * srcH * sizeof(*srcI));
+    srcU32 = malloc(srcW * srcH * sizeof(*srcU32));
     for (int px = 0; px < srcW * srcH; ++px) {
-        srcI[px] = px;
-        for (int pxPrev = 0; pxPrev < px; ++pxPrev) {
-            if (memcmp(
-                    src + px * bytesPerPixel,
-                    src + pxPrev * bytesPerPixel,
-                    bytesPerPixel) == 0) {
-                srcI[px] = pxPrev;
-                break;
-            }
-        }
+        srcU32[px] = 0;
+        // @TODO cover the big-endian case
+        memcpy(srcU32 + px, src + px * bytesPerPixel, bytesPerPixel);
     }
 
-    dstI = malloc(dstW * dstH * sizeof(*dstI));
-    if (wfc_generate(srcW, srcH, srcI, dstW, dstH, dstI) != 0) {
+    dstU32 = malloc(dstW * dstH * sizeof(*dstU32));
+    if (wfc_generate(srcW, srcH, srcU32, dstW, dstH, dstU32) != 0) {
         ret = 1;
         goto cleanup;
     }
 
     for (int px = 0; px < dstW * dstH; ++px) {
-        memcpy(
-            dst + px * bytesPerPixel,
-            src + dstI[px] * bytesPerPixel,
-            bytesPerPixel);
+        memcpy(dst + px * bytesPerPixel, dstU32 + px, bytesPerPixel);
     }
 
 cleanup:
-    free(dstI);
-    free(srcI);
+    free(dstU32);
+    free(srcU32);
 
     return ret;
 }

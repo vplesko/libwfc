dd38ac7e3e617983aee74b17ea472f43391dc00f
Vladimir Pleskonjic <21056458+vplesko@users.noreply.github.com>
2023-07-30 09:34:44 +0100

    Add a license

--- wfc.h
+++ wfc.h
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2023 Vladimir Pleskonjic
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include <assert.h>
 #include <float.h>
 #include <math.h>
@@ -15,7 +39,6 @@
 // public declarations
 
 // @TODO input validation
-// @TODO (maybe) return a code for client error instead of asserting
 
 enum {
     wfc_optFlipH = 1 << 1,

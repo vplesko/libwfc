126aa4cf874c32d4585e7dd7170fe0fb25cc7015
vplesko <21056458+vplesko@users.noreply.github.com>
2023-08-28 10:26:59 +0200

    Minor

--- gui/main.c
+++ gui/main.c
@@ -31,7 +31,6 @@ const int srcDstGap = 2;
 const int zoomMin = 1, zoomMax = 8;
 const int cursorSizeMin = 1, cursorSizeMax = 9;
 
-// @TODO Allow resetting of the entire process.
 const char *instructions =
     "Controls:\n"
     "\t+            - Zoom in\n"
